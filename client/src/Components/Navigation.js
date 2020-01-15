a17918109e631ddf449b4d3ddd50ab3d71c1425e
tjforeman <tylerforeman92@gmail.com>
2020-01-14 19:01:32 -0500

    nav built

---
+++ client/src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/Navigation.css'
+
+const Navigation = () => {
+    return(
+        <div className='nav-wrapper'>
+        <a>About Me</a>
+        <a>Projects</a>
+        <a>Contact</a>
+
+        </div>
+    )
+}
+
+
+export default Navigation
