900cf163048f083b82d506735a50591640abb1dc
tjforeman <tylerforeman92@gmail.com>
2020-01-21 16:08:42 -0500

    more styles

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -5,26 +5,26 @@ const Projects = () => {
 return(
 <div className = 'projects-container'>
 <div className = 'project-card'>
-<h2>Role: </h2>
-<h2>Title: </h2>
+<h2>Title: Handy-App </h2>
+<h2>Role: Full Stack Developer </h2>
 <img className = 'project-img' src={require('../img/handy.png')}/>
 <p>Description: </p>
 </div>
 <div className = 'project-card'>
-<h2>Role: </h2>
-<h2>Title: </h2>
+<h2>Title: Brav </h2>
+<h2>Role: Team Lead </h2>
 <img className = 'project-img' src={require('../img/brav.png')} />
 <p>Description: </p>
 </div>
 <div className = 'project-card'>
-<h2>Role: </h2>
-<h2>Title: </h2>
+<h2>Title: Grantlify </h2>
+<h2>Role: Team Lead </h2>
 <img className = 'project-img' src={require('../img/grantlify.png')} />
 <p>Description: </p>
 </div>
 <div className = 'project-card'>
-<h2>Role: </h2>
-<h2>Title: </h2>
+<h2>Title: Labby </h2>
+<h2>Role: Team Lead </h2>
 <img />
 <p>Description: </p>
 </div>
