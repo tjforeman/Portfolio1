6cf0b5e72ac906ba984958a25882474167b761fb
tjforeman <tylerforeman92@gmail.com>
2020-01-25 23:55:35 -0500

    added description

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -8,7 +8,7 @@ return(
 <h2>Handy-App </h2>
 <h2>Full Stack Developer </h2>
 <img className = 'project-img' src={require('../img/handy.png')}/>
-<p>Description: </p>
+<p>An application built to allow Homeowners to purchase the services of Contractors. A Homeowner can post a project along with pictures, details, and any other sort of desired information. A contractor can then make a bid on the project, and give any other information, such as material costs and ect. A time can be selected for the contractor to work on the project, and payment can be given from the customer directly to the the Contractor.</p>
 </div>
 <div className = 'project-card'>
 <h2>Brav </h2>
