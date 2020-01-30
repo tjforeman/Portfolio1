f5bb4e5c74a00a85c0374e5b614b9d32144d5c32
tjforeman <tylerforeman92@gmail.com>
2020-01-30 14:23:13 -0500

    small change to about me

--- client/src/Components/Banner.js
+++ client/src/Components/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
 return(
     <div className = 'banner-container' >
         <div className = 'about'>
-             <p name="about-me" >My name is Tyler Foreman. I enjoy working on scalable web applications. I'm comfortable working on any aspect of a web application. I currently live in Charlotte, North Carolina but am willing to relocate anywhere. I find working on world class product teams very enjoyable, and would like to someday help build a popular product for users to enjoy. </p>
+             <p name="about-me" >My name is Tyler Foreman. I like working on scalable web applications. I'm comfortable working on any aspect of a web application. I currently live in Charlotte, North Carolina but am willing to relocate anywhere. I find working on world class product teams very enjoyable, and would like to someday help build a popular product for users to benefit from. </p>
         
         <img className='profile-pic' src={require('../img/profle_pic.jpg')}  />
         </div>
