9a84d3b1f5531d31ab3ec54c91e771abfa6d9103
tjforeman <tylerforeman92@gmail.com>
2020-01-30 13:34:17 -0500

    updated links

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -7,21 +7,25 @@ return(
 <div className = 'line-1'>
 <div className = 'project-card'>
 <h2>Handy-App - Connecting contractors and homeowners to get work done. </h2>
-<h5>Full Stack Developer</h5>
+{/* <h5>Full Stack Developer</h5> */}
 <p>ReactJS, NodeJS, Redux, PostgresSQL, Auth0, Stripe Connect, Clouninary, Material-UI</p>
 <img className = 'project-img' src={require('../img/handy.png')}/>
-<button>See the Product</button>
-<button>See the Code</button>
+<div className = 'links'>
+<a href='https://handyapp.netlify.com/'>See the product</a>
+<a href = 'https://github.com/labs13-handy-app'>See the Code</a>
+</div>
 </div>
 <div className = 'project-card'>
 <h2>Brav - Connecting mediators and customers to manage conflict. </h2>
-<h5>Team Lead </h5>
+{/* <h5>Team Lead </h5> */}
 <p>
 ReactJS, NodeJS, Stripe Connect, PostgresSQL, Firebase Auth and Storage, Material-UI
 </p>
 <img className = 'project-img' src={require('../img/brav.png')} />
-<button>See the Product</button>
-<button>See the Code</button>
+<div className = 'links'>
+<a href = 'https://www.beabravone.com/'>See the Product</a>
+<a href = 'https://github.com/labs14-brav'>See the Code</a>
+</div>
 </div>
 </div>
 <div className = 'line-2'>
@@ -30,16 +34,20 @@ ReactJS, NodeJS, Stripe Connect, PostgresSQL, Firebase Auth and Storage, Materia
 <h5>Team Lead</h5>
 <p> ReactJS, Nodejs, Firebase Authentication, PostgreSQL, Material-UI </p>
 <img className = 'project-img' src={require('../img/grantlify.png')} />
-<button>See the Product</button>
-<button>See the Code</button>
+<div className = 'links'>
+<a href = 'https://www.grantlify.com/'>See the Product</a>
+<a href = 'https://github.com/tjforeman/startup-grant-database-pt5-fe'> See the Code</a>
+</div>
 </div>
 <div className = 'project-card'>
 <h2>Labby - Automating the creation of teams for Lambda Labs </h2>
 <h5>Team Lead</h5>
 <p>AWS Lex, AWS RDS, AWS Lambda, AWS api gateway, AWS s3-bucket, PostgresSQL</p>
 <img className = 'project-img' src={require('../img/labby.png')}/>
-<button>See the Product</button>
-<button>See the Code</button>
+<div className = 'links'>
+<a href = 'https://lambdalabby.com/'>See the Product</a>
+<a href = 'https://github.com/Lambda-School-Labs/labby-be'>See the Code</a>
+</div>
 </div>
 </div>
 </div>)
