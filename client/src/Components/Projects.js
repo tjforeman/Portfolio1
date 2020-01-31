75bbdafa012b5d88ca44a6e896b55ff9c8761fe6
tjforeman <tylerforeman92@gmail.com>
2020-01-30 19:33:32 -0500

    cleaned up code

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -8,7 +8,6 @@ return(
 <div className = 'line-1'>
 <div className = 'project-card'>
 <h2>Handy-App - Connecting contractors and homeowners to get work done. </h2>
-{/* <h5>Full Stack Developer</h5> */}
 <p>ReactJS, NodeJS, Redux, PostgresSQL, Auth0, Stripe Connect, Clouninary, Material-UI</p>
 <img className = 'project-img' src={require('../img/handy.png')}/>
 <div className = 'links'>
@@ -18,7 +17,6 @@ return(
 </div>
 <div className = 'project-card'>
 <h2>Brav - Connecting mediators and customers to manage conflict. </h2>
-{/* <h5>Team Lead </h5> */}
 <p>
 ReactJS, NodeJS, Stripe Connect, PostgresSQL, Firebase Auth and Storage, Material-UI
 </p>
@@ -32,7 +30,6 @@ ReactJS, NodeJS, Stripe Connect, PostgresSQL, Firebase Auth and Storage, Materia
 <div className = 'line-2'>
 <div className = 'project-card'>
 <h2>Grantlify - Creating a place for grant seekers, and givers to collaborate </h2>
-{/* <h5>Team Lead</h5> */}
 <p> ReactJS, Nodejs, Firebase Authentication, PostgreSQL, Material-UI </p>
 <img className = 'project-img' src={require('../img/grantlify.png')} />
 <div className = 'links'>
@@ -42,7 +39,6 @@ ReactJS, NodeJS, Stripe Connect, PostgresSQL, Firebase Auth and Storage, Materia
 </div>
 <div className = 'project-card'>
 <h2>Labby - Automating the creation of teams for Lambda Labs </h2>
-{/* <h5>Team Lead</h5> */}
 <p>AWS Lex, AWS RDS, AWS Lambda, AWS api gateway, AWS s3-bucket, PostgresSQL</p>
 <img className = 'project-img' src={require('../img/labby.png')}/>
 <div className = 'links'>
